2fc483421d89f29bbaee7914aa97d64656efed49
Gutierri Barboza <me@gutierri.me>
2017-11-17 19:47:34 -0200

    viewset qrcode remove update and delete method rest api

--- qrcode/views.py
+++ qrcode/views.py
@@ -2,14 +2,14 @@ from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from .models import Qrcode
 from .serializers import QrcodeSerializer
 
 
-class QrcodeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
+class QrcodeViewSet(mixins.CreateModelMixin,
+                    mixins.ListModelMixin,
+                    mixins.RetrieveModelMixin,
+                   viewsets.GenericViewSet):
     queryset = Qrcode.objects.all()
     serializer_class = QrcodeSerializer

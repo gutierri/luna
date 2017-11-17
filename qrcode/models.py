8f053d0524b855badcfeb84afbea3fafadd765a3
Gutierri Barboza <me@gutierri.me>
2017-11-17 19:40:49 -0200

    changes for django rest framework
    - change to viewset
    - date auto now
    - routers via django rest framework urls

--- qrcode/models.py
+++ qrcode/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Qrcode(models.Model):
     device = models.CharField(max_length=20)
     data = models.CharField(max_length=42)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now_add=True)
--- qrcode/urls.py
+++ qrcode/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import QrcodeList, QrcodeDetail
+from .views import QrcodeViewSet
 
 
-urlpatterns = [
-    url(r'^qrcodes/$', QrcodeList.as_view()),
-    url(r'^qrcodes/(?P<id>[0-9]+)/$', QrcodeDetail.as_view())
-]
+router = routers.SimpleRouter()
+router.register(r'qrcodes', QrcodeViewSet)
+
+urlpatterns = router.urls
